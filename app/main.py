92f603e14de9e0ffaba037b3ff774037f2da050d
Pedrovargas10 <pedrofvargas10@gmail.com>
2026-05-10 16:26:10 -0300

    fix: corrigido erro de sintaxe

--- app/main.py
+++ app/main.py
@@ -134,4 +134,3 @@ def alterar_pagamento(nome: str = Path(..., description="Nome do jogador")):
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
-ue)
